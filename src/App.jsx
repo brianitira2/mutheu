302a1f4729ce723a294c8af1e41447469702dd2f
gigokomrade apps <zetechuniversity254@gmail.com>
2023-11-13 21:02:11 +0000

    commit number six

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Welcome';
 import Details from './Details'
-import Contact from './Contact';
+import Footer from './Footer';
 import LoanAmount from './LoanAmount';
 import Congrats from './Congrats';
 
@@ -11,14 +11,18 @@ const App = () => {
   return (
     <Router>
       <div>
-        
 
+     
+
+     
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Details" element={<Details />} />
           <Route path="/Congrats" element={<Congrats />} />
           <Route path="/LoanAmount" element={<LoanAmount />} />
         </Routes>
+
+        <Footer/>
       </div>
     </Router>
   );
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <p>Secure Prime Loans was built by Brian Itira</p>
+        <p>&copy; 2023</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
