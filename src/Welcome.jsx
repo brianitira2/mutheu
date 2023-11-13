0bdd896c3479eff738c5fa7cd5985da586949552
gigokomrade apps <zetechuniversity254@gmail.com>
2023-11-13 19:42:31 +0000

    second commit

---
+++ src/Welcome.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Welcome.css'
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1 className='welcome-header-text'>SECURE  PRIME LOANS</h1>
+      <h2 className='welcome-header2-text'>where your financial dreams come true</h2>
+      <p className='welcome-p-text'>click on the button below to start your loan application with us today</p>
+     
+      <div className='btn'>
+
+      <button className='welcome-btn'>click to apply</button>
+
+      </div>
+      </div>
+  )
+}
+
+export default Welcome
